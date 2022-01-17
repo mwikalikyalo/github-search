5e92d50ceb56813b18d4d0be9f9b2221d83b514d
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-17 23:46:18 +0300

    Add the user profile

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,10 +1,12 @@
 export class User {
   constructor(
     public name: string,
+    public username: string,
     public avatar_url: string,
     public bio: string,
     public followers: string,
     public following: string,
-    public repos: number){
+    public repos: number,
+    public update: Date){
   }
 }
