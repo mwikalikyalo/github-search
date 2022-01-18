8d5a3084291fb892d05ec149de0093c3126031c0
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-19 00:16:30 +0300

    Add the user details and repo details

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileServiceService } from '../profile-service.service';
 import { Repository } from '../repository';
 import { User } from '../user';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  providers: [ProfileServiceService]
 })
 
 export class ProfileComponent implements OnInit {
-    User!: User;
-    Repository!: Repository;
+  user!:User;
+  repos!:Repository[];
 
-  constructor(private http: HttpClient) {
-
-   }
-
-  ngOnInit(): void {
+  searchUserName(username:string){
+    this.users_Request.userRequest(username);
+    this.user = this.users_Request.user;
+    this.users_Request.repoRequest(username).subscribe(data=>{
+      this.repos = data
+    })
+    
   }
 
+  constructor( private profileservice:ProfileServiceService) { }
+
+
+  ngOnInit(): void {}
+
 }
+
