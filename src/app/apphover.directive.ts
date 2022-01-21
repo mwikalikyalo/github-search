9cd6ea961d668755724dd4f980b8625364ef1934
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-01-22 00:27:04 +0300

    Final updates to the directives

--- src/app/apphover.directive.ts
+++ src/app/apphover.directive.ts
@@ -7,13 +7,13 @@ export class ApphoverDirective {
 
   constructor(private elem:ElementRef) { }
 
-  @HostListener("mouseover") onMouseOver(){
-    this.backgroundColor("rgb(29, 61, 98)")
-    this.elem.nativeElement.style.color ="white"
-  }
   @HostListener("mouseleave") onMouseLeave(){
-    this.backgroundColor("white")
+    this.backgroundColor("#d8e2dc")
     this.elem.nativeElement.style.color = "black"
+  }
+  @HostListener("mouseover") onMouseOver(){
+    this.backgroundColor("#ffd60a")
+    this.elem.nativeElement.style.color ="grey"
   }
   private backgroundColor(action:string){
     this.elem.nativeElement.style.backgroundColor = action;
